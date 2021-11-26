4fc2bb45bb6bd134c87e65ffe47d4d94f447cb4f
Brian Brennglass <>
2021-11-26 09:20:18 -0500

    redux provider added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import combinedReducers from './redux/reducers/combinedReducers';
 
+const store = createStore(combinedReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store = {store}>
       <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
