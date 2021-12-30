24bce3b505ef97e3dc1f7b43f260aa768a87f14a
Brian Brennglass <>
2021-12-30 09:56:13 -0500

    saving before merge

--- src/Components/Docs/Docs.js
+++ src/Components/Docs/Docs.js
@@ -1,12 +1,10 @@
-import React,{useState} from 'react';
+import React from 'react';
 import './Docs.css';
 import GridGenerator from '../Grid/Grid';
 import CssEditor from '../CssEditor/CssEditor';
-import {CaretUpOutlined} from '@ant-design/icons'
 
 
 const Docs = ({docGrids, index})=>{
-    const [isHover, setIsHover] = useState(false);
     return(
         <div className = "carousel">
             {
@@ -19,10 +17,10 @@ const Docs = ({docGrids, index})=>{
                                     
                                     <h4>{exmp.title}</h4>
                                     <hr />
+                                    <span className = 'text'>{exmp.text}</span>
                                     <div className = "editor-code">
                                       <CssEditor index = {index} isDocs = {true} style = {exmp} docGridIndex = {i}/>
                                     </div>
-                                    <span className = 'text'>{exmp.text}</span>
                                 </div>
                                 <div className = "card card-grid">
                                     <GridGenerator 
--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Grid.css';
 import styled from 'styled-components'
-import { v4 as uuidv4 } from 'uuid';
 const GRID_COLORS = ['#dc97a9', '#f2cb7c', '#edaf88', '#d3bfb6', '#addad7', '#e5a88b', '#cf9683', '#cf7956', '#d16644', '#878f84', '#eedac2'];
 
 
